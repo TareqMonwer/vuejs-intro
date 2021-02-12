0980d70c4e8bec55bc4ebd91968afac31a862825
tareqmonwer <tareqmonwer137@gmail.com>
2021-02-13 01:16:37 +0600

    events and methods in vue.

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             bookName: 'Automate the boring stuffs with python',
+            price: 10,
+        }
+    },
+    methods: {
+        changeBook(newBook) {
+            this.bookName = newBook
         }
     }
 })
